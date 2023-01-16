4d5b533bc259a58d2c366aa6d7bc6738847a33e7
Miguel Penha <miguel.penha1@gmail.com>
2023-01-16 17:05:36 -0300

    Ajeitando algumas coisas

--- styles/pages/teachers/index.ts
+++ styles/pages/teachers/index.ts
@@ -7,13 +7,13 @@ export const Container = styled.div`
 `
 
 export const Title = styled.h1`
-    font-size: 3.5vw;
+    font-size: 2.5vw;
     margin-top: 3rem;
     text-align: center;
     margin-bottom: 4rem;
     color: ${props => props.theme.primary};
 
     @media screen and (max-width: 900px) {
-        font-size: 6.5vw;
+        font-size: 7.5vw;
     }
 `
--- styles/pages/teachers/sign.ts
+++ styles/pages/teachers/sign.ts
@@ -8,14 +8,14 @@ export const Container = styled.div`
 `
 
 export const Title = styled.h1`
-    font-size: 3.5vw;
+    font-size: 2.5vw;
     margin-top: 3rem;
     text-align: center;
     margin-bottom: 4rem;
     color: ${props => props.theme.primary};
 
     @media screen and (max-width: 900px) {
-        font-size: 6.5vw;
+        font-size: 7.5vw;
     }
 `
 
