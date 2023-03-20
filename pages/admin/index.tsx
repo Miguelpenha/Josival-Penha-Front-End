5c1b97eb8f86bdb31c45425d3602746e757a34a1
Miguel Penha <miguel.penha1@gmail.com>
2023-03-20 02:30:16 -0300

    Teste

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -34,6 +34,11 @@ function Admin() {
                     <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"/>
                 </svg>
             </ButtonLink>
+            <ButtonLink href="admin/notify" title="Notificar">
+                <svg width="2.8em" height="2.8em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/>
+                </svg>
+            </ButtonLink>
         </ContainerPD>
     </>
 }
