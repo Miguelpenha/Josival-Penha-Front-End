cf73038f43e1298300c5fdf74e05e24e3d90ddf4
Miguel Penha <miguel.penha1@gmail.com>
2023-03-25 17:29:57 -0300

    Update index.tsx

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,15 +1,26 @@
-import { FC, ButtonHTMLAttributes } from 'react'
+import { FC, ButtonHTMLAttributes, useState } from 'react'
 import { Container, Text } from './style'
 import Loading from '../Loading'
 
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     loading?: boolean
+    onClick: () => Promise<void>
 }
 
-const Button: FC<IProps> = ({ disabled, loading=false, title, children, ...props }) => {
+const Button: FC<IProps> = ({ disabled, onClick, loading=false, title, children, ...props }) => {
+    const [loadingState, setLoadingState] = useState(false)
+    
+    async function handleClick() {
+        loading && setLoadingState(true)
+
+        await onClick()
+
+        loading && setLoadingState(false)
+    }
+    
     return (
-        <Container loading={loading} disabled={Boolean(disabled) || loading} {...props}>
-            {loading ? <Loading/> : children}
+        <Container onClick={handleClick} loading={loadingState} disabled={Boolean(disabled) || loadingState} {...props}>
+            {(loading && loadingState) ? <Loading/> : children}
             <Text>{title}</Text>
         </Container>
     )
