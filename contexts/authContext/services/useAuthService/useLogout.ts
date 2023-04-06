1f8f42c8199ada0b637e7eae43c580b214bb8bcc
Miguel Penha <miguel.penha1@gmail.com>
2023-04-06 09:04:00 -0300

    Update useLogout.ts

--- contexts/authContext/services/useAuthService/useLogout.ts
+++ contexts/authContext/services/useAuthService/useLogout.ts
@@ -1,17 +1,17 @@
 import { Dispatch, SetStateAction } from 'react'
-import { destroyCookie } from 'nookies'
+import nookies from 'nookies'
 
 function useLogout(setAdminIndex: Dispatch<SetStateAction<number | null>>, setTeacherID: Dispatch<SetStateAction<string | null>>) {
     async function logoutAdmin() {
         setAdminIndex(null)
     
-        destroyCookie(undefined, process.env.NEXT_PUBLIC_NAME_COOKIE_LOGIN_ADMIN)
+        nookies.destroy(undefined, process.env.NEXT_PUBLIC_NAME_COOKIE_LOGIN_ADMIN)
     }
 
     async function logoutTeacher() {
         setTeacherID(null)
     
-        destroyCookie(undefined, process.env.NEXT_PUBLIC_NAME_COOKIE_LOGIN_TEACHER)
+        nookies.destroy(undefined, process.env.NEXT_PUBLIC_NAME_COOKIE_LOGIN_TEACHER)
     }
 
     return {
