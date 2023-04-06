c79193f753d8be113653da0d0d8f0e474641d657
Miguel Penha <miguel.penha1@gmail.com>
2023-04-06 09:15:46 -0300

    Update settings.tsx

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -6,7 +6,7 @@ import Head from 'next/head'
 import ContainerDefault from '../components/ContainerDefault'
 import { Title, Field, Label, Data, ButtonLogout } from '../styles/pages/settings'
 import Loading from '../components/Loading'
-import nookies from 'nookies'
+import nookies, { destroyCookie } from 'nookies'
 
 function Settings() {
     const admin = useAdmin()
@@ -34,7 +34,11 @@ function Settings() {
                     <Label>Logado como </Label>
                     <Data onClick={async () => await handleCopy(admin || teacher)}>{admin || teacher}</Data>
                 </Field>
-                <ButtonLogout title="Logout" onClick={logout}>
+                <ButtonLogout title="Logout" onClick={async () => {
+                    await logout()
+
+                    destroyCookie(undefined, process.env.NEXT_PUBLIC_NAME_COOKIE_LOGIN_ADMIN)
+                }}>
                     <svg width="2.3em" height="2.3em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M0 0h24v24H0z" fill="none"/>
                         <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"/>
