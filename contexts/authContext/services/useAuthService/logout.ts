6be8b8dc8440eabe132be85377fda4353442ee1c
Miguel Penha <miguel.penha1@gmail.com>
2023-04-06 07:39:09 -0300

    Update logout.ts

--- contexts/authContext/services/useAuthService/logout.ts
+++ contexts/authContext/services/useAuthService/logout.ts
@@ -1,12 +1,12 @@
 import { Dispatch, SetStateAction } from 'react'
-import { destroyCookie, } from 'nookies'
+import nookies from 'nookies'
 
 async function logout(setValue: Dispatch<SetStateAction<any>>, item: string) {
     setValue(null)
 
     console.log('logout feito', item)
 
-    destroyCookie(undefined, item)
+    nookies.destroy(undefined, item)
 }
 
 export default logout
