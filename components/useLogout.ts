13f7c10449e92c2ebadf3fb4388d9e9727e99b50
Miguel Penha <miguel.penha1@gmail.com>
2023-04-06 09:15:10 -0300

    Update useLogout.ts

--- components/useLogout.ts
+++ components/useLogout.ts
@@ -1,7 +1,6 @@
 import useType from './useType'
 import useAuth from '../contexts/authContext'
 import { useRouter } from 'next/router'
-import { destroyCookie } from 'nookies'
 
 function useLogout() {
     const type = useType()
@@ -14,10 +13,6 @@ function useLogout() {
         } else if (type == 'teacher') {
             await logoutTeacher()
         }
-
-        destroyCookie(undefined, process.env.NEXT_PUBLIC_NAME_COOKIE_LOGIN_ADMIN, {
-            path: '/'
-        })
         
         // await router.replace('/')
     }
