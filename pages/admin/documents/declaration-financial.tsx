669c97bc523bca22b26debf52858d6f5c8af2345
Miguel Penha <miguel.penha1@gmail.com>
2023-10-16 07:05:46 -0300

    Adicionando página de declaração financeira

---
+++ pages/admin/documents/declaration-financial.tsx
@@ -0,0 +1,70 @@
+import api from '../../../services/api'
+import IStudent from '../../../types/student'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
+import generateDocument from '../../../components/generateDocument'
+import { toast } from 'react-toastify'
+import Head from 'next/head'
+import ContainerDefault from '../../../components/ContainerDefault'
+import { Title, Form, Field, Label } from '../../../styles/pages/admin/documents/declaration-financial'
+import Select from '../../../components/Select'
+import Input from '../../../components/Input'
+import ButtonSubmit from '../../../components/ButtonSubmit'
+import Loading from '../../../components/Loading'
+import getServerSidePropsAuthAdmin from '../../../utils/getServerSidePropsAuthAdmin'
+
+interface IForm {
+    month: string
+    student: IStudent
+}
+
+function Documents() {
+    const { data: students } = api.get<IStudent[]>('/students')
+    const { watch, setValue, register } = useForm<IForm>()
+    const { student, month } = watch()
+    const router = useRouter()
+
+    async function handleSubmit() {
+        await generateDocument(`/students/documents/declaration-finance/${student._id}?month=${month}`, `Declaração financeira do aluno(a) ${student.name}.pdf`)
+        
+        await router.push('/admin/documents')
+
+        toast('Declaração gerada com sucesso!', {
+            type: 'success'
+        })
+    }
+    
+    return <>
+        <Head>
+            <title>Gerar declaração</title>
+        </Head>
+        <ContainerDefault back="/admin/documents">
+            <Title>Gerar declaração</Title>
+            <Form onSubmit={ev => ev.preventDefault()}>
+                {students ? <>
+                    <Select
+                        name="student"
+                        onChange={({ value: student }) => setValue('student', student)}
+                        options={students && students.map(student => ({
+                            value: student,
+                            label: student.name
+                        }))}
+                    />
+                    <Field>
+                        <Label>Adimplente até o Mês: </Label>
+                        <Input
+                            required
+                            placeholder="Mês..."
+                            {...register('month')}
+                        />
+                    </Field>
+                    <ButtonSubmit loading onClick={handleSubmit} disabled={!student} title="Gerar"/>
+                </> : <Loading size={90} weight={8} speed={0.8}/>}
+            </Form>
+        </ContainerDefault>
+    </>
+}
+
+export default Documents
+
+export const getServerSideProps = getServerSidePropsAuthAdmin
---
+++ styles/pages/admin/documents/declaration-financial.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+    width: 80%;
+    font-size: 2.5vw;
+    margin-top: 3rem;
+    text-align: center;
+    align-self: center;
+    margin-bottom: 3rem;
+    color: ${props => props.theme.primary};
+
+    @media screen and (max-width: 900px) {
+        font-size: 8vw;
+    }
+`
+
+export const Form = styled.form`
+    width: 24rem;
+    height: 24em;
+    padding: 2rem;
+    display: flex;
+    align-self: center;
+    border-radius: 10px;
+    flex-direction: column;
+    background-color: ${props => props.theme.primary};
+
+    @media screen and (max-width: 900px) {
+        width: 18rem;
+    }
+`
+
+export const Field = styled.div`
+    width: 85%;
+    display: flex;
+    margin-bottom: 12%;
+    align-self: center;
+    flex-direction: column;
+`
+
+export const Label = styled.label`
+    font-size: 1rem;
+    font-weight: bold;
+    color: ${props => props.theme.secondaryColor};
+`
