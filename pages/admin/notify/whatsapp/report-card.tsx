1450d632ca5ea512d5b4bbff5c2b1dfbb7d16f70
Miguel Penha <miguel.penha1@gmail.com>
2023-12-26 23:29:58 -0300

    Ajeitando algumas coisas

--- pages/admin/notify/whatsapp/report-card.tsx
+++ pages/admin/notify/whatsapp/report-card.tsx
@@ -10,31 +10,28 @@ import { Title, Form, ButtonSubmit } from '../../../../styles/pages/admin/notify
 import Select from '../../../../components/Select'
 import Loading from '../../../../components/Loading'
 import getServerSidePropsAuthAdmin from '../../../../utils/getServerSidePropsAuthAdmin'
+import Input from '../../../../components/Input'
 
 interface IForm {
-    unit: string
+    month: string
     student: string
 }
 
-const units = [
-    '1° unidade',
-    '2° unidade',
-    '3° unidade',
-    '4° unidade'
-]
-
 function ReportCard() {
-    const { watch, setValue } = useForm<IForm>()
-    const { student, unit } = watch()
+    const { watch, setValue, register } = useForm<IForm>()
+    const { student, month } = watch()
     const router = useRouter()
     const { data: students } = api.get<IStudent[]>('/students')
 
     async function handleSubmit() {
-        await base.get(`/notify/report-card/${student}?unit=${unit}`)
+        const telephoneIsDDD = student.replace(/-/g, ' ').replace(/\(/g, '').replace(/\)/g, '').replace(/\s+/g, '').replace(/\+/g, '')
+        const { link } = (await base.post<{ link: string }>(`/notify/whatsapp/${telephoneIsDDD.includes('55') ? telephoneIsDDD : `55${telephoneIsDDD}`}`, { month })).data
+
+        await navigator.clipboard.writeText(link)
 
         router.push('/admin/notify')
 
-        toast('Notificação enviada com sucesso!', {
+        toast('Link do Whatsapp copiado para a área de transferência!', {
            type: 'success' 
         })
     }
@@ -53,24 +50,19 @@ function ReportCard() {
                         placeholder="Nome do aluno..."
                         onChange={({ value: student }) => setValue('student', student)}
                         options={[...students.map(student => ({
-                            value: student._id,
+                            value: student.telephone,
                             label: student.name
                         })), {
                             value: 'all',
                             label: 'Todos'
                         }]}
                     />
-                    <Select
+                    <Input
                         required
-                        name="unit"
-                        placeholder="Unidade do boletim..."
-                        onChange={({ value: unit }) => setValue('unit', unit)}
-                        options={units.map((unit, index) => ({
-                            label: unit,
-                            value: index+1
-                        }))}
+                        {...register('month')}
+                        placeholder="Mês do boleto..."
                     />
-                    <ButtonSubmit loading disabled={!student || !unit} onClick={handleSubmit} title="Notificar"/>
+                    <ButtonSubmit loading disabled={!student || !month} onClick={handleSubmit} title="Notificar"/>
                 </> : <Loading size={90} weight={8} speed={0.8}/>}
             </Form>
         </ContainerDefault>
