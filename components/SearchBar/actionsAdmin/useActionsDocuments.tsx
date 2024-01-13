52085c56a435c2d0496190153fad59108347b9a4
Miguel Penha <miguel.penha1@gmail.com>
2024-01-13 01:53:37 -0300

    Update useActionsDocuments.tsx

--- components/SearchBar/actionsAdmin/useActionsDocuments.tsx
+++ components/SearchBar/actionsAdmin/useActionsDocuments.tsx
@@ -77,6 +77,19 @@ function useActionsDocuments(router: NextRouter): Action[] {
                     <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
                 </svg>
             )
+        },
+        {
+            parent: 'declarations',
+            id: 'declarationProvisionalTransfer',
+            name: 'Declaração provisória de transferência',
+            perform: () => router.push('/admin/documents/declarations/provisional-transfer'),
+            keywords: 'declaração declaration documentos documento documents document declaration provisória de transferência provisional transfer',
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
+                </svg>
+            )
         }
     ]
 }
